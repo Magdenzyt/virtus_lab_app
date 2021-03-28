104cecdd6b205196a11ab4db5446988d28a72383
MagdaWu <wumagda16@gmail.com>
2021-03-28 17:41:07 +0200

    Movies

--- src/components/characters.js
+++ src/components/characters.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import style from "./characters.module.css";
-import { Button,  Card, Accordion } from 'react-bootstrap';
+import { Button, Card, Accordion } from 'react-bootstrap';
 
 class Characters extends Component {
   constructor(props) {
@@ -10,6 +10,7 @@ class Characters extends Component {
       index: 1
     };
     this.handleClick = this.handleClick.bind(this);
+    this.handleClickMovies = this.handleClickMovies.bind(this);
   }
 
 
@@ -22,7 +23,10 @@ class Characters extends Component {
           'Content-Type': 'application/json'
         }
       })
-        .then(res => res.json())
+        .then(res => {
+          if (res.ok) return res.json();
+          return Promise.reject(res);
+        })
         .then(res => {
           this.setState({
             characters: [
@@ -36,6 +40,34 @@ class Characters extends Component {
     }
   }
 
+  callAPIMovies(url) {
+    var tab = url.split("/");
+    var id = tab[tab.length - 2];
+
+    fetch(`https://swapi.dev/api/films/${id}/`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(res => {
+        for (var i = 0; i < this.state.characters.length; i++) {
+          for (var j = 0; j < this.state.characters[i]["films"].length; j++) {
+            if (this.state.characters[i]["films"][j] == url) {
+              this.state.characters[i]["films"][j] = res["title"];
+            }
+          }
+        }
+        this.setState({
+          characters: this.state.characters
+        })
+      }).catch(err => {
+        console.error(err);
+      });
+
+  }
+
   componentDidMount() {
     this.callAPI(10);
   }
@@ -44,27 +76,40 @@ class Characters extends Component {
     this.callAPI(5);
   }
 
+  handleClickMovies(character) {
+    for (var i = 0; i < character["films"].length; i++) {
+      let url = character["films"][i];
+      if (url.startsWith("http")) {
+        this.callAPIMovies(url);
+      }
+    }
+  }
+
   render() {
     return (
       <div>
         {this.state.characters.map(co => (
           <Accordion>
             <Card>
-              <Accordion.Toggle as={Card.Header} eventKey="0">
+              <Accordion.Toggle as={Card.Header} eventKey="0" onClick={this.handleClickMovies.bind(null, co)}>
                 <h3>{co.name}</h3>
-                <br></br>
-                <pre><b>Gender:</b> {co.gender}        <b>Birth year:</b> {co.birth_year}</pre>               
+                <br />
+                <pre><b>Gender:</b> {co.gender}        <b>Birth year:</b> {co.birth_year}</pre>
               </Accordion.Toggle>
               <Accordion.Collapse eventKey="0">
                 <Card.Body>
-                <pre><b>Weight:</b> {co.mass}        <b>Height:</b> {co.height}</pre>
+                  <pre><b>Weight:</b> {co.mass}        <b>Height:</b> {co.height}</pre>
+                  <b>Movies</b>
+                  {co["films"].map(f => (
+                    <div>{f}</div>
+                  ))}
                 </Card.Body>
               </Accordion.Collapse>
             </Card>
           </Accordion>
         ))}
         <div class="btn-group my-5" role="group" aria-label="First group">
-         <Button class ="btn" variant = "outline-dark" onClick={this.handleClick}>Load more</Button>
+          <Button class="btn" variant="outline-dark" onClick={this.handleClick}>Load more</Button>
         </div>
       </div>
       /*<div className={style.text}>
